f57db713b5f80a2ec5651f8d644744b04c680e2f
Denis Bystruev <dbystruev@me.com>
2018-11-20 13:49:32 +0300

    Project as of start of 20.11.2018

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
